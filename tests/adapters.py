7e7a299b9586d3c6c9ab3d2a052494f32a30f44a
eCho514 <54626434+IamHBW@users.noreply.github.com>
2026-08-04 20:23:31 +0800

    implemented get_tokenizer

--- tests/adapters.py
+++ tests/adapters.py
@@ -4,7 +4,7 @@ import os
 from collections.abc import Iterable
 from typing import IO, Any, BinaryIO
 
-from cs336_basics.bpe import BPE_train
+from cs336_basics.bpe import BPE_train,BPE
 import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
@@ -560,7 +560,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return BPE(vocab,merges,special_tokens)
 
 
 def run_train_bpe(
